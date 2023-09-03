a9b25c7f323f23b38a8050bb87f486f7f68444ce
Utsavthebug <neupaneutsav.2001@gmail.com>
2023-09-03 19:26:44 +0545

    initial commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero'
+import Footer from './Footer'
+import PopularProducts from './PopularProducts'
+import Services from './Services'
+import SpecialOffer from './SpecialOffer'
+import CustomerReviews from './CustomerReviews'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+
+export {
+    Hero,
+    Footer,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    SuperQuality
+}
